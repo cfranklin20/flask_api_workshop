ddb18fc593be46fb268ecfb8bdc4305178129b6e
Clayton Franklin <clayton.j.franklin@nevada.unr.edu>
2020-01-31 23:45:20 -0800

    Fixing python version to 3.7

--- flask_env/bin/activate.fish
+++ flask_env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/flask_api_workshop/flask_env"
+set -gx VIRTUAL_ENV "/home/clayton/college/flask_api_workshop/flask_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(flask_env) "            
+        if test -n "(flask_env) "
             printf "%s%s" "(flask_env) " (set_color normal)
         else
             # ...Otherwise, prepend env
