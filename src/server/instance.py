941298c19874f4299973eb0e9453f1f67fb27dc6
Clayton Franklin <clayton.j.franklin@nevada.unr.edu>
2020-02-01 10:43:35 -0800

    Creating files for API

--- src/server/instance.py
+++ src/server/instance.py
@@ -1,21 +1,23 @@
 from flask import Flask
 from flask_restplus import Api, Resource, fields
-from environment.instance import environment_config
+
 
 class Server(object):
     def __init__(self):
         self.app = Flask(__name__)
-        self.api = Api(self.app, 
-            version='1.0', 
-            title='Sample Book API',
-            description='A simple Book API', 
-            doc = environment_config["swagger-url"]
-        )
+        self.api = Api(self.app,
+                       version='1.0',
+                       title='Sample Book API',
+                       description='A simple Book API',
+                       doc='/books/docs'
+                       )
+        self.app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 
     def run(self):
         self.app.run(
-                debug = environment_config["debug"], 
-                port = environment_config["port"]
-            )
+            debug=True,
+            port=5000
+        )
+
 
 server = Server()
